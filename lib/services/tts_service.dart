e89b8112f528120ccdded49a9816f35a230be483
itszack0099-droid <itszack0099@gmail.com>
2026-04-13 13:34:13 +0530

    Create tts_service.dart

---
+++ lib/services/tts_service.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class TTSService {
+  final FlutterTts _tts = FlutterTts();
+
+  Future<void> speak(String text) async {
+    await _tts.setLanguage("en-US");
+    await _tts.setSpeechRate(0.5);
+    await _tts.speak(text);
+  }
+}
