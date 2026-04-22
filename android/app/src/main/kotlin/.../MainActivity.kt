3530d54f7636b9cda72b288232340a1e8a82f69e
itszack0099-droid <itszack0099@gmail.com>
2026-04-22 18:12:27 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/.../MainActivity.kt
+++ android/app/src/main/kotlin/.../MainActivity.kt
@@ -1,6 +1,32 @@
-package com.example.app
+package com.zackify.notifier
 
-import io.flutter.embedding.android.FlutterActivity
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+
+        super.onCreate(
+            savedInstanceState
+        )
+
+        if (Build.VERSION.SDK_INT >= 33) {
+
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission
+                        .POST_NOTIFICATIONS
+                ),
+                1
+            )
+
+        }
+
+    }
+
 }
